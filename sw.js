dc2b866e9f9ccc2349d6f3c5294d401fc473bf3e
Silver Jaanus <silver.jaanus@gmail.com>
2026-09-10 09:59:34 +0300

    sw cache v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='korrutaja-v8';
+const C='korrutaja-v9';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
